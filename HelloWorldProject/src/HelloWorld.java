c10b78d13373925b67b1a0b7875c4fa54535e946
Rashid Gaitov <rashidgaitov@gmail.com>
2022-10-04 09:59:34 +0300

    Commit Java

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -2,7 +2,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World, I love you!");
 		
 		int firstVariable = 25;
 		int secondVariable = 35;		
