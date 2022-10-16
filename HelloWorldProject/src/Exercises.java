ef1a79d168ee61ea6713b9a826ac2a36fdfa2500
Rashid Gaitov <rashidgaitov@gmail.com>
2022-10-16 16:11:26 +0300

    Exercise 2. Use of Scanner

--- HelloWorldProject/src/Exercises.java
+++ HelloWorldProject/src/Exercises.java
@@ -2,16 +2,29 @@
 // Date: 16.10.2022
 // SWE 501.01 Course
 
+import java.util.Scanner;
+
 public class Exercises {
 	public static void main(String[] args) {
 	
-		// Compute the area of a circle
+		// Exercise 1: Compute the area of a circle
 		
-		double area;
-		double pi = 3.1415;
-		int radius = 7;
+//		double area;
+//		double pi = 3.1415;
+//		int radius = 7;
+//		
+//		area = pi * radius * radius;
+//		System.out.println("The area of a circle with the radius " + radius + " is equal to " + area);
 		
-		area = pi * radius * radius;
-		System.out.println("The area of a circle with the radius " + radius + " is equal to " + area);
+		
+		// Exercise 2: Area with the use of Scanner
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter radius: ");
+		double radius = input.nextDouble();
+		double area = radius * radius * 3.14;
+		
+		System.out.println("The area is " + area);
 	}
 }
